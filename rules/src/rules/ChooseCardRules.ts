e57a08783a71f629a989e75b44f917cbdc2844aa
Patrick Beaumé <p.beaume@gmail.com>
2023-04-08 21:34:50 +0200

    fix: effect order on card choose is always: card effect, hero recruitment then trade coin

--- rules/src/rules/ChooseCardRules.ts
+++ rules/src/rules/ChooseCardRules.ts
@@ -1,75 +1,80 @@
-import { NidavellirRules } from './NidavellirRules';
-import { Player, PlayerId } from '../state/Player';
-import Move from '../moves/Move';
-import { getCardsInTavern, isOnPlayerBoard } from '../utils/location.utils';
-import MoveType from '../moves/MoveType';
-import { isLocatedCoin } from '../utils/player.utils';
-import { InTavern } from '../state/LocatedCard';
-import { MoveCard } from '../moves/MoveCard';
-import { Cards } from '../cards/Cards';
-import { EffectType } from '../effects/EffectType';
-import GameState from '../state/GameState';
-import GameView from '../state/view/GameView';
-import { isExchangeCoin } from '../utils/coin.utils';
-import { getChooseCardMove, onChooseCard } from '../utils/card.utils';
-import { applyThrud } from '../utils/hero.utils';
+import { NidavellirRules } from './NidavellirRules'
+import { Player, PlayerId } from '../state/Player'
+import Move from '../moves/Move'
+import { getCardsInTavern, isOnPlayerBoard } from '../utils/location.utils'
+import MoveType from '../moves/MoveType'
+import { isLocatedCoin } from '../utils/player.utils'
+import { InTavern } from '../state/LocatedCard'
+import { MoveCard } from '../moves/MoveCard'
+import { Cards } from '../cards/Cards'
+import { EffectType } from '../effects/EffectType'
+import GameState from '../state/GameState'
+import GameView from '../state/view/GameView'
+import { isExchangeCoin } from '../utils/coin.utils'
+import { getChooseCardMove, onChooseCard } from '../utils/card.utils'
+import { applyThrud } from '../utils/hero.utils'
 
 class ChooseCardRules extends NidavellirRules {
-  player: Player;
+  player: Player
 
   constructor(game: GameState | GameView, player: Player) {
-    super(game);
-    this.player = player;
+    super(game)
+    this.player = player
   }
 
   getLegalMoves(playerId: PlayerId): Move[] {
     if (playerId !== this.player.id || this.player.playedCard !== undefined) {
-      return [];
+      return []
     }
 
-    const currentTavern = this.game.tavern;
-    const tavernCards = getCardsInTavern(this.game).filter((c) => (c.location as InTavern).tavern === currentTavern);
+    const currentTavern = this.game.tavern
+    const tavernCards = getCardsInTavern(this.game).filter((c) => (c.location as InTavern).tavern === currentTavern)
 
-    return tavernCards.map((c) => getChooseCardMove(this.game, this.player, c.id!));
+    return tavernCards.map((c) => getChooseCardMove(this.game, this.player, c.id!))
   }
 
   play(move: Move) {
     switch (move.type) {
       case MoveType.MoveCard:
-        return this.chooseCard(move);
+        return this.chooseCard(move)
     }
 
-    return super.play(move);
+    return super.play(move)
   }
 
   chooseCard(move: MoveCard) {
-    const thrudMoves = applyThrud(this.game, this.player, move);
+    const thrudMoves = applyThrud(this.game, this.player, move)
     if (thrudMoves.length) {
-      return thrudMoves;
+      return thrudMoves
     }
 
-    onChooseCard(this.game, this.player, move.id!, 'age');
 
-    const tavern = this.game.tavern;
+    const tavern = this.game.tavern
     const playerCoin = this.game.coins.find(
       (coin) =>
         isOnPlayerBoard(coin.location) && coin.location.index === tavern && coin.location.player === this.player.id
-    );
+    )
+
+    const card = Cards[move.id!]
+
+    // First, card effect
+    if (card.effects?.length) {
+      this.player.effects.push(...JSON.parse(JSON.stringify(card.effects)))
+    }
+
+    // Then, potential hero recruiting
+    onChooseCard(this.game, this.player, move.id!, 'age')
 
     // Trade is only triggered if the player has played a 0-value coin
+    // Finally, trade coin if needed
     if (playerCoin && isLocatedCoin(playerCoin) && isExchangeCoin(playerCoin)) {
       this.player.effects.push({
-        type: EffectType.TRADE_COIN,
-      });
+        type: EffectType.TRADE_COIN
+      })
     }
 
-    const card = Cards[move.id!];
-    if (card.effects?.length) {
-      this.player.effects.push(...JSON.parse(JSON.stringify(card.effects)));
-    }
-
-    return [];
+    return []
   }
 }
 
-export { ChooseCardRules };
+export { ChooseCardRules }
