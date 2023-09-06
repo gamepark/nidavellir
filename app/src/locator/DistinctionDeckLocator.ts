417cc123b0e9ccbbad0ecf751ec105ec70b982f5
Patrick Beaumé <p.beaume@gmail.com>
2023-09-06 21:14:48 +0200

    place distinction component depending on their distinction

--- app/src/locator/DistinctionDeckLocator.ts
+++ app/src/locator/DistinctionDeckLocator.ts
@@ -1,14 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { ItemContext, LineLocator } from '@gamepark/react-game'
-import { PlayerId } from "@gamepark/nidavellir/player/Player";
-import { MaterialType } from "@gamepark/nidavellir/material/MaterialType";
-import { LocationType } from "@gamepark/nidavellir/material/LocationType";
-import { cardDescription } from "../material/DwarfCardDescription";
-import { Coordinates, MaterialItem } from "@gamepark/rules-api";
+import { PlayerId } from '@gamepark/nidavellir/player/Player'
+import { MaterialType } from '@gamepark/nidavellir/material/MaterialType'
+import { LocationType } from '@gamepark/nidavellir/material/LocationType'
+import { cardDescription } from '../material/DwarfCardDescription'
+import { Coordinates, MaterialItem } from '@gamepark/rules-api'
+import { Distinction } from '@gamepark/nidavellir/material/Distinction'
 
 export class DistinctionDeckLocator extends LineLocator<PlayerId, MaterialType, LocationType> {
 
-  getDelta(item: MaterialItem, { rules }: ItemContext) {
+  getDelta(item: MaterialItem, context: ItemContext): Partial<Coordinates>{
+    const { rules } = context
     const players = rules.players.length
     const y = cardDescription.getSize(item.id).height - (players > 3? 3: 2)
     return { y }
@@ -19,5 +21,31 @@ export class DistinctionDeckLocator extends LineLocator<PlayerId, MaterialType, 
     const y = players > 3? -24: -29
     const x = players > 3? 57: 33
     return { x, y, z: 0 }
+  }
+
+  getItemIndex(item: MaterialItem, context: ItemContext): number {
+    const { type, rules } = context
+    if (type === MaterialType.Gem) {
+      return this.getItemIndex(
+        rules.material(MaterialType.Distinction).id(Distinction.CrownJeweler).getItem()!,
+        { ...context, type: MaterialType.Distinction }
+      )
+    }
+
+    if (type === MaterialType.Card) {
+      return this.getItemIndex(
+        rules.material(MaterialType.Distinction).id(Distinction.KingsGreatArmorer).getItem()!,
+        { ...context, type: MaterialType.Distinction }
+      )
+    }
+
+    if (type == MaterialType.Coin) {
+      return this.getItemIndex(
+        rules.material(MaterialType.Distinction).id(Distinction.HuntingMaster).getItem()!,
+        { ...context, type: MaterialType.Distinction },
+      )
+    }
+
+    return super.getItemIndex(item, context)
   }
 }
