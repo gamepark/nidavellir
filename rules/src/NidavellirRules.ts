466994a5406a3f7de31d93165e5113a855ea2fef
Romain Fromi <romain.fromi@gmail.com>
2023-09-25 14:41:37 +0200

    Remove dead code

--- rules/src/NidavellirRules.ts
+++ rules/src/NidavellirRules.ts
@@ -1,4 +1,4 @@
-import { CompetitiveScore, MaterialGame, MaterialMove, MaterialMoveRandomized, MaterialMoveView, rankByScore, SecretMaterialRules } from '@gamepark/rules-api'
+import { CompetitiveScore, MaterialGame, MaterialMove, rankByScore, SecretMaterialRules } from '@gamepark/rules-api'
 import { PlayerId } from './player/Player'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
@@ -8,8 +8,7 @@ import { locationsStrategies } from './configuration/LocationStrategies'
 import { Score } from './rules/helpers/Score'
 
 export class NidavellirRules extends SecretMaterialRules<PlayerId, MaterialType, LocationType>
-  implements CompetitiveScore<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId>
-{
+  implements CompetitiveScore<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId> {
 
   rankPlayers(playerA: PlayerId, playerB: PlayerId): number {
     return rankByScore(playerA, playerB, this.getScore.bind(this))
@@ -22,9 +21,4 @@ export class NidavellirRules extends SecretMaterialRules<PlayerId, MaterialType,
   rules = rules
   locationsStrategies = locationsStrategies
   hidingStrategies = hidingStrategies
-
-  play(move: MaterialMoveRandomized<PlayerId, MaterialType, LocationType> | MaterialMoveView<PlayerId, MaterialType, LocationType>): MaterialMove<PlayerId, MaterialType, LocationType>[] {
-    const moves = super.play(move)
-    return moves
-  }
 }
