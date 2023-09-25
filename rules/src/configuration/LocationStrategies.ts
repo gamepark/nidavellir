6998e943c7622e4c8fc3bfb9917847edc9031f5f
Romain Fromi <romain.fromi@gmail.com>
2023-09-25 14:06:21 +0200

    Replace OnlyAddPositiveStrategy by FillGapStrategy

--- rules/src/configuration/LocationStrategies.ts
+++ rules/src/configuration/LocationStrategies.ts
@@ -1,26 +1,25 @@
 import { LocationType } from '../material/LocationType'
-import { PositiveSequenceStrategy } from '@gamepark/rules-api'
+import { FillGapStrategy, PositiveSequenceStrategy } from '@gamepark/rules-api'
 import { MaterialType } from '../material/MaterialType'
-import { TreasureLocationStrategy } from "./TreasureLocationStrategy";
-import { OnlyAddPositiveStrategy } from "./OnlyAddPositiveStrategy";
+import { TreasureLocationStrategy } from './TreasureLocationStrategy'
 
 export const locationsStrategies = {
   [MaterialType.Card]: {
     [LocationType.Age1Deck]: new PositiveSequenceStrategy(),
     [LocationType.Age2Deck]: new PositiveSequenceStrategy(),
-    [LocationType.Tavern]: new OnlyAddPositiveStrategy(),
-    [LocationType.HeroesDeck]: new OnlyAddPositiveStrategy(),
+    [LocationType.Tavern]: new FillGapStrategy(),
+    [LocationType.HeroesDeck]: new FillGapStrategy(),
     [LocationType.Hand]: new PositiveSequenceStrategy(),
     [LocationType.Discard]: new PositiveSequenceStrategy(),
     [LocationType.Army]: new PositiveSequenceStrategy(),
     [LocationType.DistinctionsDeck]: new PositiveSequenceStrategy(),
-    [LocationType.CommandZone]: new PositiveSequenceStrategy(),
+    [LocationType.CommandZone]: new PositiveSequenceStrategy()
   },
   [MaterialType.Coin]: {
     [LocationType.Treasure]: new TreasureLocationStrategy(),
     [LocationType.Hand]: new PositiveSequenceStrategy(),
     [LocationType.Discard]: new PositiveSequenceStrategy(),
-    [LocationType.DistinctionsDeck]: new PositiveSequenceStrategy(),
+    [LocationType.DistinctionsDeck]: new PositiveSequenceStrategy()
   },
   [MaterialType.Gem]: {
     [LocationType.PlayerBoard]: new PositiveSequenceStrategy()
