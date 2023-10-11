97a06fae868cf604b86934203b43fb8edc731f14
Romain Fromi <romain.fromi@gmail.com>
2023-10-11 17:40:56 +0200

    Add TimeLimit

--- rules/src/NidavellirRules.ts
+++ rules/src/NidavellirRules.ts
@@ -1,20 +1,26 @@
-import { CompetitiveScore, MaterialGame, MaterialMove, SecretMaterialRules } from '@gamepark/rules-api'
-import { PlayerId } from './player/Player'
+import { CompetitiveScore, MaterialGame, MaterialMove, SecretMaterialRules, TimeLimit } from '@gamepark/rules-api'
+import { hidingStrategies } from './configuration/HidingStrategies'
+import { locationsStrategies } from './configuration/LocationStrategies'
+import { rules } from './configuration/RuleDefinitions'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
-import { hidingStrategies } from './configuration/HidingStrategies'
-import { rules } from './configuration/RuleDefinitions'
-import { locationsStrategies } from './configuration/LocationStrategies'
+import { PlayerId } from './player/Player'
 import { Score } from './rules/helpers/Score'
+import { RuleId } from './rules/RuleId'
 
 export class NidavellirRules extends SecretMaterialRules<PlayerId, MaterialType, LocationType>
-  implements CompetitiveScore<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId> {
+  implements CompetitiveScore<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId>,
+    TimeLimit<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId> {
+
+  rules = rules
+  locationsStrategies = locationsStrategies
+  hidingStrategies = hidingStrategies
 
   getScore(playerId: PlayerId) {
     return new Score(this.game, playerId).score
   }
 
-  rules = rules
-  locationsStrategies = locationsStrategies
-  hidingStrategies = hidingStrategies
+  giveTime(): number {
+    return this.game.rule?.id === RuleId.Bids ? 45 : 20
+  }
 }
