d67a32949320b661db1676049fb9a796ea84b36c
Patrick Beaumé <p.beaume@gmail.com>
2023-10-24 18:33:31 +0200

    fix number of card on setup

--- app/src/material/DwarfCardDescription.tsx
+++ app/src/material/DwarfCardDescription.tsx
@@ -1,8 +1,78 @@
 /** @jsxImportSource @emotion/react */
 import { Card, CardDeck } from '@gamepark/nidavellir/cards/Cards'
 import { CardDescription } from '@gamepark/react-game'
-import Images from '../images/Images'
-import { DwarfCardRules } from './rules/DwarfCardRules'
+import { DwarfCardRules  } from './rules/DwarfCardRules'
+import Blacksmith1 from '../images/cards/age-card/blacksmith/Blacksmith1.jpg'
+import Blacksmith2 from '../images/cards/age-card/blacksmith/Blacksmith2.jpg'
+import BlacksmithKingsGreatArmorer from '../images/cards/age-card/blacksmith/BlacksmithKingsGreatArmorer.jpg'
+import ExplorerGrade5_1 from '../images/cards/age-card/explorer/ExplorerGrade5_1.jpg'
+import ExplorerGrade5_2 from '../images/cards/age-card/explorer/ExplorerGrade5_2.jpg'
+import ExplorerGrade6_1 from '../images/cards/age-card/explorer/ExplorerGrade6_1.jpg'
+import ExplorerGrade6_2 from '../images/cards/age-card/explorer/ExplorerGrade6_2.jpg'
+import ExplorerGrade7_1 from '../images/cards/age-card/explorer/ExplorerGrade7_1.jpg'
+import ExplorerGrade7_2 from '../images/cards/age-card/explorer/ExplorerGrade7_2.jpg'
+import ExplorerGrade8_1 from '../images/cards/age-card/explorer/ExplorerGrade8_1.jpg'
+import ExplorerGrade8_2 from '../images/cards/age-card/explorer/ExplorerGrade8_2.jpg'
+import ExplorerGrade9_1 from '../images/cards/age-card/explorer/ExplorerGrade9_1.jpg'
+import ExplorerGrade9_2 from '../images/cards/age-card/explorer/ExplorerGrade9_2.jpg'
+import ExplorerGrade10_1 from '../images/cards/age-card/explorer/ExplorerGrade10_1.jpg'
+import ExplorerGrade10_2 from '../images/cards/age-card/explorer/ExplorerGrade10_2.jpg'
+import ExplorerGrade11_1 from '../images/cards/age-card/explorer/ExplorerGrade11_1.jpg'
+import ExplorerGrade11_2 from '../images/cards/age-card/explorer/ExplorerGrade11_2.jpg'
+import ExplorerGrade12_1 from '../images/cards/age-card/explorer/ExplorerGrade12_1.jpg'
+import ExplorerGrade12_2 from '../images/cards/age-card/explorer/ExplorerGrade12_2.jpg'
+import Hunter1 from '../images/cards/age-card/hunter/Hunter1.jpg'
+import Hunter2 from '../images/cards/age-card/hunter/Hunter2.jpg'
+import MinerGrade0_1 from '../images/cards/age-card/miner/MinerGrade0_1.jpg'
+import MinerGrade0_2 from '../images/cards/age-card/miner/MinerGrade0_2.jpg'
+import MinerGrade1_1 from '../images/cards/age-card/miner/MinerGrade1_1.jpg'
+import MinerGrade1_2 from '../images/cards/age-card/miner/MinerGrade1_2.jpg'
+import MinerGrade2_1 from '../images/cards/age-card/miner/MinerGrade2_1.jpg'
+import MinerGrade2_2 from '../images/cards/age-card/miner/MinerGrade2_2.jpg'
+import RoyalOffering3 from '../images/cards/age-card/royal-offering/RoyalOffering3.jpg'
+import RoyalOffering5 from '../images/cards/age-card/royal-offering/RoyalOffering5.jpg'
+import WarriorGrade3_1 from '../images/cards/age-card/warrior/WarriorGrade3_1.jpg'
+import WarriorGrade3_2 from '../images/cards/age-card/warrior/WarriorGrade3_2.jpg'
+import WarriorGrade4_1 from '../images/cards/age-card/warrior/WarriorGrade4_1.jpg'
+import WarriorGrade4_2 from '../images/cards/age-card/warrior/WarriorGrade4_2.jpg'
+import WarriorGrade5_1 from '../images/cards/age-card/warrior/WarriorGrade5_1.jpg'
+import WarriorGrade5_2 from '../images/cards/age-card/warrior/WarriorGrade5_2.jpg'
+import WarriorGrade6_1 from '../images/cards/age-card/warrior/WarriorGrade6_1.jpg'
+import WarriorGrade6_2 from '../images/cards/age-card/warrior/WarriorGrade6_2.jpg'
+import WarriorGrade7_1 from '../images/cards/age-card/warrior/WarriorGrade7_1.jpg'
+import WarriorGrade7_2 from '../images/cards/age-card/warrior/WarriorGrade7_2.jpg'
+import WarriorGrade8_1 from '../images/cards/age-card/warrior/WarriorGrade8_1.jpg'
+import WarriorGrade8_2 from '../images/cards/age-card/warrior/WarriorGrade8_2.jpg'
+import WarriorGrade9_1 from '../images/cards/age-card/warrior/WarriorGrade9_1.jpg'
+import WarriorGrade9_2 from '../images/cards/age-card/warrior/WarriorGrade9_2.jpg'
+import WarriorGrade10_1 from '../images/cards/age-card/warrior/WarriorGrade10_1.jpg'
+import WarriorGrade10_2 from '../images/cards/age-card/warrior/WarriorGrade10_2.jpg'
+
+import Age1Back from '../images/cards/age-card/age1-back.jpg'
+import Age2Back from '../images/cards/age-card/age2-back.jpg'
+import HeroBack from '../images/cards/heroes/back.jpg'
+
+import Aegur from '../images/cards/heroes/Aegur.jpg'
+import Aral from '../images/cards/heroes/Aral.jpg'
+import Astrid from '../images/cards/heroes/Astrid.jpg'
+import Bonfur from '../images/cards/heroes/Bonfur.jpg'
+import Dagda from '../images/cards/heroes/Dagda.jpg'
+import DwergAesir from '../images/cards/heroes/DwergAesir.jpg'
+import DwergBergelmir from '../images/cards/heroes/DwergBergelmir.jpg'
+import DwergJungir from '../images/cards/heroes/DwergJungir.jpg'
+import DwergSigmir from '../images/cards/heroes/DwergSigmir.jpg'
+import DwergYmir from '../images/cards/heroes/DwergYmir.jpg'
+import Grid from '../images/cards/heroes/Grid.jpg'
+import Hourya from '../images/cards/heroes/Hourya.jpg'
+import Idunn from '../images/cards/heroes/Idunn.jpg'
+import Kraal from '../images/cards/heroes/Kraal.jpg'
+import Lokdur from '../images/cards/heroes/Lokdur.jpg'
+import Skaa from '../images/cards/heroes/Skaa.jpg'
+import Tarah from '../images/cards/heroes/Tarah.jpg'
+import Thrud from '../images/cards/heroes/Thrud.jpg'
+import Uline from '../images/cards/heroes/Uline.jpg'
+import Ylud from '../images/cards/heroes/Ylud.jpg'
+import Zoral from '../images/cards/heroes/Zoral.jpg'
 
 export class DwarfCardDescription extends CardDescription {
   width = 5.4
@@ -10,85 +80,79 @@ export class DwarfCardDescription extends CardDescription {
   borderRadius = 0.3
 
   backImages = {
-    [CardDeck.Age1]: Images.Age1Back,
-    [CardDeck.Age2]: Images.Age2Back,
-    [CardDeck.Hero]: Images.HeroBack
+    [CardDeck.Age1]: Age1Back,
+    [CardDeck.Age2]: Age2Back,
+    [CardDeck.Hero]: HeroBack
   }
 
   images = {
-    [Card.WarriorGrade3_1]: Images.WarriorGrade3_1,
-    [Card.WarriorGrade3_2]: Images.WarriorGrade3_2,
-    [Card.WarriorGrade4_1]: Images.WarriorGrade4_1,
-    [Card.WarriorGrade4_2]: Images.WarriorGrade4_2,
-    [Card.WarriorGrade5_1]: Images.WarriorGrade5_1,
-    [Card.WarriorGrade5_2]: Images.WarriorGrade5_2,
-    [Card.WarriorGrade6_1]: Images.WarriorGrade6_1,
-    [Card.WarriorGrade6_2]: Images.WarriorGrade6_2,
-    [Card.WarriorGrade7_1]: Images.WarriorGrade7_1,
-    [Card.WarriorGrade7_2]: Images.WarriorGrade7_2,
-    [Card.WarriorGrade8_1]: Images.WarriorGrade8_1,
-    [Card.WarriorGrade8_2]: Images.WarriorGrade8_2,
-    [Card.WarriorGrade9_1]: Images.WarriorGrade9_1,
-    [Card.WarriorGrade9_2]: Images.WarriorGrade9_2,
-    [Card.WarriorGrade10_1]: Images.WarriorGrade10_1,
-    [Card.WarriorGrade10_2]: Images.WarriorGrade10_2,
+    [Card.WarriorGrade3_1]: WarriorGrade3_1, // 0
+    [Card.WarriorGrade3_2]: WarriorGrade3_2, // 1
+    [Card.WarriorGrade4_1]: WarriorGrade4_1, // 0
+    [Card.WarriorGrade4_2]: WarriorGrade4_2, // 1
+    [Card.WarriorGrade5_1]: WarriorGrade5_1, // 0
+    [Card.WarriorGrade5_2]: WarriorGrade5_2, // 1
+    [Card.WarriorGrade6_1]: WarriorGrade6_1, // 1
+    [Card.WarriorGrade6_2]: WarriorGrade6_2, // 1
+    [Card.WarriorGrade7_1]: WarriorGrade7_1, // 0
+    [Card.WarriorGrade7_2]: WarriorGrade7_2, // 1
+    [Card.WarriorGrade8_1]: WarriorGrade8_1, // 0
+    [Card.WarriorGrade8_2]: WarriorGrade8_2, // 1
+    [Card.WarriorGrade9_1]: WarriorGrade9_1, // 0
+    [Card.WarriorGrade9_2]: WarriorGrade9_2, // 1
+    [Card.WarriorGrade10_1]: WarriorGrade10_1, // 0
+    [Card.WarriorGrade10_2]: WarriorGrade10_2, // 1
+    [Card.Hunter1]: Hunter1, // 4
+    [Card.Hunter2]: Hunter2, // 4
+    [Card.MinerGrade0_1]: MinerGrade0_1, // 2
+    [Card.MinerGrade0_2]: MinerGrade0_2, // 1
+    [Card.MinerGrade1_1]: MinerGrade1_1, // 1
+    [Card.MinerGrade1_2]: MinerGrade1_2, // 2
+    [Card.MinerGrade2_1]: MinerGrade2_1, // 1
+    [Card.MinerGrade2_2]: MinerGrade2_2, // 1
+    [Card.Blacksmith1]: Blacksmith1, // 5
+    [Card.Blacksmith2]: Blacksmith2, // 5
+    [Card.ExplorerGrade5_1]: ExplorerGrade5_1, // 0
+    [Card.ExplorerGrade5_2]: ExplorerGrade5_2, // 1
+    [Card.ExplorerGrade6_1]: ExplorerGrade6_1, // 0
+    [Card.ExplorerGrade6_2]: ExplorerGrade6_2, // 1
+    [Card.ExplorerGrade7_1]: ExplorerGrade7_1, // 0
+    [Card.ExplorerGrade7_2]: ExplorerGrade7_2, // 1
+    [Card.ExplorerGrade8_1]: ExplorerGrade8_1, // 0
+    [Card.ExplorerGrade8_2]: ExplorerGrade8_2, // 1
+    [Card.ExplorerGrade9_1]: ExplorerGrade9_1, // 0
+    [Card.ExplorerGrade9_2]: ExplorerGrade9_2, // 1
+    [Card.ExplorerGrade10_1]: ExplorerGrade10_1, // 0
+    [Card.ExplorerGrade10_2]: ExplorerGrade10_2, // 1
+    [Card.ExplorerGrade11_1]: ExplorerGrade11_1, // 0
+    [Card.ExplorerGrade11_2]: ExplorerGrade11_2, // 1
+    [Card.ExplorerGrade12_1]: ExplorerGrade12_1, // 0
+    [Card.ExplorerGrade12_2]: ExplorerGrade12_2, // 1
+    [Card.RoyalOffering3]: RoyalOffering3, // 0
+    [Card.RoyalOffering5]: RoyalOffering5, // 3
+    [Card.BlacksmithKingsGreatArmorer]: BlacksmithKingsGreatArmorer,
 
-    [Card.Hunter1]: Images.Hunter1,
-    [Card.Hunter2]: Images.Hunter2,
-
-    [Card.MinerGrade0_1]: Images.MinerGrade0_1,
-    [Card.MinerGrade0_2]: Images.MinerGrade0_2,
-    [Card.MinerGrade1_1]: Images.MinerGrade1_1,
-    [Card.MinerGrade1_2]: Images.MinerGrade1_2,
-    [Card.MinerGrade2_1]: Images.MinerGrade2_1,
-    [Card.MinerGrade2_2]: Images.MinerGrade2_2,
-
-    [Card.Blacksmith1]: Images.Blacksmith1,
-    [Card.Blacksmith2]: Images.Blacksmith2,
-
-    [Card.ExplorerGrade5_1]: Images.ExplorerGrade5_1, // Explorer 5
-    [Card.ExplorerGrade5_2]: Images.ExplorerGrade5_2, // Explorer 5
-    [Card.ExplorerGrade6_1]: Images.ExplorerGrade6_1, // Explorer 6
-    [Card.ExplorerGrade6_2]: Images.ExplorerGrade6_2, // Explorer 6
-    [Card.ExplorerGrade7_1]: Images.ExplorerGrade7_1, // Explorer 7
-    [Card.ExplorerGrade7_2]: Images.ExplorerGrade7_2, // Explorer 7
-    [Card.ExplorerGrade8_1]: Images.ExplorerGrade8_1, // Explorer 8
-    [Card.ExplorerGrade8_2]: Images.ExplorerGrade8_2, // Explorer 8
-    [Card.ExplorerGrade9_1]: Images.ExplorerGrade9_1, // Explorer 9
-    [Card.ExplorerGrade9_2]: Images.ExplorerGrade9_2, // Explorer 9
-    [Card.ExplorerGrade10_1]: Images.ExplorerGrade10_1, // Explorer 10
-    [Card.ExplorerGrade10_2]: Images.ExplorerGrade10_2, // Explorer 10
-    [Card.ExplorerGrade11_1]: Images.ExplorerGrade11_1, // Explorer 11
-    [Card.ExplorerGrade11_2]: Images.ExplorerGrade11_2, // Explorer 11
-    [Card.ExplorerGrade12_1]: Images.ExplorerGrade12_1, // Explorer 12
-    [Card.ExplorerGrade12_2]: Images.ExplorerGrade12_2, // Explorer 12
-
-    [Card.RoyalOffering3]: Images.RoyalOffering3,
-    [Card.RoyalOffering5]: Images.RoyalOffering5,
-
-    [Card.BlacksmithKingsGreatArmorer]: Images.BlacksmithKingsGreatArmorer,
-
-    [Card.Bonfur]: Images.Bonfur,
-    [Card.Aegur]: Images.Aegur,
-    [Card.Dagda]: Images.Dagda,
-    [Card.Aral]: Images.Aral,
-    [Card.Kraal]: Images.Kraal,
-    [Card.Tarah]: Images.Tarah,
-    [Card.Lokdur]: Images.Lokdur,
-    [Card.Zoral]: Images.Zoral,
-    [Card.Idunn]: Images.Idunn,
-    [Card.Hourya]: Images.Hourya,
-    [Card.Astrid]: Images.Astrid,
-    [Card.DwergYmir]: Images.DwergYmir,
-    [Card.DwergAesir]: Images.DwergAesir,
-    [Card.DwergSigmir]: Images.DwergSigmir,
-    [Card.DwergJungir]: Images.DwergJungir,
-    [Card.DwergBergelmir]: Images.DwergBergelmir,
-    [Card.Thrud]: Images.Thrud,
-    [Card.Ylud]: Images.Ylud,
-    [Card.Skaa]: Images.Skaa,
-    [Card.Uline]: Images.Uline,
-    [Card.Grid]: Images.Grid
+    [Card.Bonfur]: Bonfur,
+    [Card.Aegur]: Aegur,
+    [Card.Dagda]: Dagda,
+    [Card.Aral]: Aral,
+    [Card.Kraal]: Kraal,
+    [Card.Tarah]: Tarah,
+    [Card.Lokdur]: Lokdur,
+    [Card.Zoral]: Zoral,
+    [Card.Idunn]: Idunn,
+    [Card.Hourya]: Hourya,
+    [Card.Astrid]: Astrid,
+    [Card.DwergYmir]: DwergYmir,
+    [Card.DwergAesir]: DwergAesir,
+    [Card.DwergSigmir]: DwergSigmir,
+    [Card.DwergJungir]: DwergJungir,
+    [Card.DwergBergelmir]: DwergBergelmir,
+    [Card.Thrud]: Thrud,
+    [Card.Ylud]: Ylud,
+    [Card.Skaa]: Skaa,
+    [Card.Uline]: Uline,
+    [Card.Grid]: Grid
   }
 
   rules = DwarfCardRules
