67c454a3bd9ba352aafe5899ff3948f586adf0eb
Romain Fromi <romain.fromi@gmail.com>
2024-07-16 13:49:14 +0200

    Adapt code to the new Bot API from rules-api

--- app/src/tutorial/TutorialAi.ts
+++ app/src/tutorial/TutorialAi.ts
@@ -3,9 +3,9 @@ import { MaterialGame, MaterialMove } from '@gamepark/rules-api'
 import { PlayerId } from '@gamepark/nidavellir/player/Player'
 import { MaterialType } from '@gamepark/nidavellir/material/MaterialType'
 import { LocationType } from '@gamepark/nidavellir/material/LocationType'
-import { NidavellirDummy } from '@gamepark/nidavellir/dummy/NidavellirDummy'
+import { NidavellirBot } from '@gamepark/nidavellir/bot/NidavellirBot'
 
 export const ai: GameAI<MaterialGame<PlayerId, MaterialType, LocationType>, MaterialMove<PlayerId, MaterialType, LocationType>, PlayerId>
-  = (game: MaterialGame<PlayerId, MaterialType, LocationType>, bot: PlayerId): Promise<MaterialMove[]> => {
-  return new NidavellirDummy().getRandomMove(game, bot)
+  = (game: MaterialGame<PlayerId, MaterialType, LocationType>, playerId: PlayerId): Promise<MaterialMove[]> => {
+  return Promise.resolve(new NidavellirBot(playerId).run(game))
 }
