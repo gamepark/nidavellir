d772e35f1333d58c75819d5ab2fb14509dac1259
Romain Fromi <romain.fromi@gmail.com>
2024-08-08 11:20:15 +0200

    Use getParentItem instead of deprecated getParentItemId

--- app/src/material/PlayerBoardDescription.tsx
+++ app/src/material/PlayerBoardDescription.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import board from '../images/board/player-board.png'
-import { BoardDescription, MaterialContext } from '@gamepark/react-game'
 import { LocationType } from '@gamepark/nidavellir/material/LocationType'
+import { tokenSpaces } from '@gamepark/nidavellir/material/PlayerBoardSpace'
+import { BoardDescription, MaterialContext } from '@gamepark/react-game'
 import { Location, MaterialItem } from '@gamepark/rules-api'
-import { tokenSpaces } from "@gamepark/nidavellir/material/PlayerBoardSpace";
+import board from '../images/board/player-board.png'
 import { PlayerBoardHelp } from './rules/PlayerBoardHelp'
 
 class PlayerBoardDescription extends BoardDescription {
@@ -12,11 +12,15 @@ class PlayerBoardDescription extends BoardDescription {
   ratio = 514 / 968
 
   getStaticItems({ rules: { players } }: MaterialContext) {
-    return players.map((p) => ({ id: p, location: { id: p, type: LocationType.Table, player: p }}))
+    return players.map(player => this.getPlayerBoard(player))
   }
 
-  getLocations(item: MaterialItem): Location<number, number>[] {
-    return tokenSpaces.map<Location>((space) => ({ id: space, type: LocationType.PlayerBoard, player: item.location.player  }))
+  getPlayerBoard(player: number) {
+    return { location: { id: player, type: LocationType.Table, player } }
+  }
+
+  getLocations(item: MaterialItem): Location[] {
+    return tokenSpaces.map<Location>((space) => ({ id: space, type: LocationType.PlayerBoard, player: item.location.player }))
   }
 
   help = PlayerBoardHelp
